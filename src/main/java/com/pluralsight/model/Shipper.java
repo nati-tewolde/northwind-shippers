ce95219e5b44d2ceb05600ec077ae55ea39a9579
nati-tewolde <natisnewemail@gmail.com>
2025-12-02 13:33:39 -0500

    Add Shipper model class

---
+++ src/main/java/com/pluralsight/model/Shipper.java
@@ -0,0 +1,32 @@
+package com.pluralsight.model;
+
+public class Shipper {
+    private int shipperId;
+    private String companyName;
+    private String phone;
+
+    public Shipper(int shipperId, String companyName, String phone) {
+        this.shipperId = shipperId;
+        this.companyName = companyName;
+        this.phone = phone;
+    }
+
+    public int getShipperId() {
+        return shipperId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "ShipperID: " + shipperId +
+                "Company Name: " + companyName +
+                "Phone: " + phone;
+    }
+}
